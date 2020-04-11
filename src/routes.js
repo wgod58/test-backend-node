860b722cff4c25305c1b918fcd0b5efe40f96c9f
zedPai <weichih.pai@imersive.com>
2020-04-11 19:40:06 +0800

    Remove uselsee code

--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,8 @@
 import express from 'express'
 import versionHealthcheck from 'version-healthcheck'
 import expressHealthcheck from 'express-healthcheck'
-import * as restaurantController from 'controllers/restaurant'
-import * as bookingController from 'controllers/booking'
 import { postEmail } from 'controllers/email.controller'
 const router = express.Router()
-
-/**
- * @param {function} handlerFunction handler function
- */
-const handlerResponse = handlerFunction => async (req, res) => {
-  req.body = { ...req.query, ...req.params, ...req.body }
-  const result = await handlerFunction(req.body)
-  // Clone the result because the delete object function will remove the memory value
-  const cloneResult = { ...result }
-  const { status = 500 } = cloneResult
-  delete cloneResult.status
-  if (Object.keys(cloneResult).length > 0)
-    return res.status(status).json(cloneResult)
-  return res.status(status).json()
-}
 
 /* Monitoring */
 router.get('/up', expressHealthcheck())
